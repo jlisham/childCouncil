8c07f16bd4cb0c30102f8cca0d4fa713dfdf4d7b
Jodi <jlisham@hotmail.com>
2017-12-08 10:37:10 -0600

    included features styling

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,24 +1,27 @@
-var gulp=require("gulp"),
-    watch=require("gulp-watch"),
-    browserSync=require('browser-sync').create();
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
+
   browserSync.init({
-    notify:false,
-    server:{
-      baseDir:"app"
+    notify: false,
+    server: {
+      baseDir: "app"
     }
   });
-    watch('./app/index.html', function(){
-      browserSync.reload();
-    });
 
-    watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('cssInject');
-    });
+  watch('./app/index.html', function() {
+    browserSync.reload();
   });
 
-  gulp.task('cssInject', ['styles'], function(){
-    return gulp.src('./app/temp/styles/styles.css')
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
-  });
+});
