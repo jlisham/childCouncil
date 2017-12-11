ce40e640000f68d732aa2da745c2242abfa4e1f9
Jodi <jlisham@hotmail.com>
2017-12-10 19:47:48 -0600

    modal complete

---
+++ app/assets/scripts/modules/modal.js
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+
+class Modal{
+  constructor(){
+    this.openModalBtn=$('.open-modal');
+    this.modal=$('.modal');
+    this.closeModalBtn=$('.modal__close');
+    this.events();
+  }
+
+  events(){
+    //openModal
+    this.openModalBtn.click(this.openModal.bind(this));
+    //closeModal
+    this.closeModalBtn.click(this.closeModal.bind(this));
+    //any keypress
+    $(document).keyup(this.keyPressHandler.bind(this));
+
+  }
+
+  keyPressHandler(e){
+    if (e.keyCode=="27"){this.closeModal();}
+
+  }
+
+  openModal(){
+    this.modal.addClass('modal--visible');
+    return false;
+
+  }
+
+  closeModal(){
+
+    this.modal.removeClass('modal--visible');
+
+  }
+
+}
+export default Modal;
