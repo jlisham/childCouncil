6f2fe8b23e59b435e2faf69c4a8e7912acaadddf
Jodi <jlisham@hotmail.com>
2017-12-18 22:42:17 -0600

    done

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,1 +1,56 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    imgMin=require('gulp-imagemin'),
+    del=require('del'),
+    usemin=require('gulp-usemin'),
+    rev=require('gulp-rev'),
+    cssnano=require('gulp-cssnano'),
+    uglify=require('gulp-uglify'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('previewDist', function(){
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  });
+});
+
+gulp.task('delDistFolder', function (){
+  return del('./dist');
+});
+
+gulp.task('copyFiles',['delDistFolder'], function(){
+  var pathsToCopy=[
+    './app/**/*',
+    '!./app/index.html',
+    '!./app/assets/images/**',
+    '!./app/assets/scripts/**',
+    '!./app/assets/styles/**',
+    '!./app/temp',
+    '!./app/temp/**',
+  ]
+  return gulp.src(pathsToCopy)
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('optimizeImages',['delDistFolder', 'icons'], function(){
+  return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+  .pipe(imgMin({
+    progressive:true,
+    interlaced:true,
+    multipass:true
+  }))
+  .pipe(gulp.dest('./dist/assets/images'));
+});
+
+gulp.task('usemin',['delDistFolder', 'styles', 'scripts'], function(){
+  return gulp.src('./app/index.html/')
+  .pipe(usemin({
+    css:[function(){return rev()}, function(){return cssnano()}],
+    js:[function(){return rev()}, function(){return uglify()}]
+  }))
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['delDistFolder', 'copyFiles', 'optimizeImages', 'usemin']);
